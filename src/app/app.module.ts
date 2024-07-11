624948a75ba0de96693616687ef546ace8b0c1d0
Demircan Çelik <demircancelik24@gmail.com>
2024-07-11 10:19:17 +0300

    missing files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { KrediService } from './kredi.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule, FormsModule
   ],
-  providers: [],
+  providers: [KrediService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
