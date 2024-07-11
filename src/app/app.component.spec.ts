54c9f66b1b69cbba6a9e2050e7ba4b3048d69151
Demircan Çelik <demircancelik24@gmail.com>
2024-07-11 10:32:09 +0300

    fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'employeemanagerapp'`, () => {
+  it(`should have as title 'ticarikrediodemeapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('employeemanagerapp');
+    expect(app.title).toEqual('ticarikrediodemeapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('employeemanagerapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ticarikrediodemeapp app is running!');
   });
 });
